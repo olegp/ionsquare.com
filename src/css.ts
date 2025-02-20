c1871e38c8bfe40deae6ba86b4cefb36df893b42
wrmk <elmakan@gmail.com>
2025-02-20 16:17:04 +0600

    add theme toggler

--- src/css.ts
+++ src/css.ts
@@ -15,3 +15,19 @@ import * as path from "path";
   const cssContent = await response.text();
   await fs.writeFile(file, cssContent);
 })();
+
+
+(async function () {
+  const url =
+    "https://cdn.jsdelivr.net/npm/@tabler/core/dist/css/tabler-marketing.min.css";
+  const dir = path.join(process.cwd(), "site", "assets", "css");
+  const file = path.join(dir, "tabler-marketing.css");
+
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Failed to download CSS: ${response.status}`);
+  }
+
+  const cssContent = await response.text();
+  await fs.writeFile(file, cssContent);
+})();
