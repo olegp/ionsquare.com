4b43e8b71b2e5104822a35c247dd10e0b29f7558
wrmk <elmakan@gmail.com>
2025-02-28 15:01:38 +0600

    rewrite esbuild building

---
+++ esbuild.watch.ts
@@ -0,0 +1,17 @@
+import * as esbuild from 'esbuild';
+
+async function watch() {
+  const ctx = await esbuild.context({
+    entryPoints: ['./src/index.js'],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    target: ['es2018'],
+    outfile: './site/assets/js/script.js',
+  });
+
+  await ctx.watch();
+  console.log('Watching for changes...');
+}
+
+watch();
