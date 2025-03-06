f3f97bcbb46984f1bd6cd62a92a3f28431ff0ded
wrmk <elmakan@gmail.com>
2025-03-06 08:29:34 +0600

    refactored images too, started to process post images

--- src/images.ts
+++ src/images.ts
@@ -3,31 +3,40 @@ import yaml from "js-yaml";
 import sharp from "sharp";
 
 interface Reference {
-  name: string;
   icon: string;
   iconProcessed: boolean;
   [key: string]: any;
 }
 
-async function processReferenceIcon(file_url: string) {
-  const output = await sharp(`./site/${file_url}`)
-    .resize(40, 40, { fit: "cover" })
-    .jpeg({ quality: 80 })
-    .toBuffer();
-
-  await fs.writeFile(`./site/${file_url}.processed`, output);
-}
-
-(async function main() {
+async function processReferenceImages() {
   const yamlContent = await fs.readFile("site/_data/references.yml", "utf8");
   const references = yaml.load(yamlContent) as Reference[];
 
   for (const reference of references) {
     if (reference.iconProcessed) continue;
 
-    await processReferenceIcon(reference.icon);
+    const output = await sharp(`./site/${reference.icon}`)
+    .resize(40, 40, { fit: "cover" })
+    .jpeg({ quality: 80 })
+    .toBuffer();
+
+    await fs.writeFile(`./site/${reference.icon}.processed`, output);
+
     reference.iconProcessed = true;
   }
 
   await fs.writeFile("site/_data/references.yml", yaml.dump(references), "utf8");
+}
+
+async function processPostImages() {
+  const posts = await fs.readdir("site/_posts");
+  for (const post of posts) {
+    const postHTML = await fs.readFile(`site/_posts/${post}`, "utf8");
+    console.log(postHTML);
+  }
+}
+
+(async function main() {
+  await processReferenceImages();
+  await processPostImages();
 })();
